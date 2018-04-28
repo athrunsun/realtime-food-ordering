4cc2ed8b418cf8d87658bf2f423ef8a568a50f00
okampfer <okampfer@gmail.com>
2018-04-28 14:52:49 +0800

    Add model syncing in beforeStart hook.

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,13 @@
 module.exports = app => {
     app.beforeStart(async () => {
         const room = await app.redis.get(app.config.defaultRoomName);
+        
         if (!room) {
             await app.redis.set(app.config.defaultRoomName, app.config.defaultRoomName);
         }
+
+        // Sync model to db: https://github.com/eggjs/egg-sequelize#sync-model-to-db
+        // "sync" method of Sequelize class: http://docs.sequelizejs.com/class/lib/sequelize.js~Sequelize.html#instance-method-sync
+        await app.model.sync();
     });
 };
